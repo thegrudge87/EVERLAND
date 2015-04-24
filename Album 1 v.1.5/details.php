73bea014636c9fdbe683fc9ca5d1108b819202da
vj <villyjord@yahoo.com>
2015-04-24 22:06:46 +0200

    Some small fixes before v.2

---
+++ Album 1 v.1.5/details.php
@@ -0,0 +1,28 @@
+<?php include('./title.inc.php');  ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset=utf-8">
+<title>Bulgarian nature<?php if (isset($title)) {echo "&#8212;{$title}";} ?></title>
+<link href="styles/journey.css" rel="stylesheet" type="text/css" media="screen">
+</head>
+
+<body>
+<div id="header">
+    <h1>Japan Journey </h1>
+</div>
+<div id="wrapper">
+    <?php include('./menu.inc_01.php'); ?>
+    <div id="maincontent">
+        <h2>Article Details Go Here  </h2>
+      <p>Proba 1</p>
+        <p>Proba 2</p>
+        <p>Proba 3</p>
+        <p>Proba 4</p>
+        <p>Proba 5</p>
+        <p><a href="blog.php">Back to the blog </a></p>
+    </div>
+    <?php include('./footer.inc_01.php'); ?>
+</div>_
+</body>
+</html>
